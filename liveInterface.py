115a65ddba746ff3524fc7c125165c06726fe288
col <bruck.jp@gmail.com>
2018-12-07 14:30:09 -0500

    added api call to interface

---
+++ liveInterface.py
@@ -0,0 +1,74 @@
+import json
+import sys
+#import grabandread.py
+from tkinter import *
+import tkinter.messagebox as box
+import requests
+#def pullCurrency():
+#atm this function only pulls the currency information with Euro as base ###
+
+r = requests.get('https://api.exchangeratesapi.io/latest')
+#provide unicode object     info = r.text
+#####uncomment to provide datacheck    print(info)
+self = r.json()
+
+#info = "usethis.json"
+#file = open(info, 'r')
+#n = file.read()
+#self = json.loads(n)
+rate = self['rates']
+data = rate.values()
+print(data)
+
+window = Tk()
+window.title('Currency Calculator')
+label = Label(window, text = 'Currency Calculator')
+label.pack(padx = 200 , pady = 50)
+frame = Frame(window)
+entry = Entry(frame)
+
+listbox = Listbox(frame, exportselection=False)
+lb = Listbox(frame)
+
+#listbox = Listbox(master)
+#listbox.pack()
+#lb = Listbox(selectmode=EXTENDED)
+
+#for rates in rate:
+#    listbox.insert(END, '{}: {}'.format(rates, rate[rates]))
+#    self.listbox.insert(END, key)
+#self.data = data
+#self.lb.delete(0, END) # clear
+
+for rates in rate:
+    listbox.insert(END, '{}: {}'.format(rates, rate[rates]))
+def conversion():
+
+###called by button click.
+
+
+    detail = entry.get()
+    magic = listbox.get(listbox.curselection())
+    print(rate.values())
+    print(type(magic))
+    selec = magic[0:5]
+    multiplier = float(magic.lstrip(magic[0:5]))
+
+    print(detail)
+    jello = float(detail)
+    print(multiplier)
+    jelloMulti = multiplier * jello
+
+    print(jelloMulti)
+
+    box.showinfo('selec', detail + "  " + selec + ' is {} euro'  .format(jelloMulti))
+    return
+
+btn = Button(frame, text = 'Calculate', command = conversion)
+btn.pack(side = RIGHT, padx = 5)
+entry.pack(side = RIGHT, padx = 10, pady = 10)
+listbox.pack(side= LEFT)
+frame.pack(padx = 30, pady = 30)
+
+
+window.mainloop()
